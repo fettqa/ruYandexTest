e965ad7d708026917bccd7db8d4ee14e35016164
fettqa <fettqa@gmail.com>
2020-01-28 14:35:02 +0300

    UPD2: corrections, comparing of dates

--- src/test/java/ru/ya/test/UsageFirstTest.java
+++ src/test/java/ru/ya/test/UsageFirstTest.java
@@ -7,18 +7,6 @@ import org.junit.Test;
 import ru.ya.test.widgets.MainSearchingPage;
 import ru.ya.test.widgets.SearchedResultsPage;
 
-/* Сценарий 1:
-         •       Для сайта https://ya.ru/ создать следующие сценарии:
-         •       Открыть страницу https://ya.ru/
-         •       Ввести в строку поиска текст "Основы автоматизации тестирования"
-         •       Нажать кнопку "Найти"
-         •       Проверить, что вернулись результаты поиска
-
-         Сценарий 2:
-         •       Открыть страницу https://yandex.ru/
-         •       Проверить, что на загруженной странице отображается блок с новостями и блок с погодой
-         •       Проверить, что дата на загруженной странице соответствует текущей */
-
 public class UsageFirstTest {
     private final MainSearchingPage mainSearchingPage = new MainSearchingPage();
     private final String request = "Основы автоматизации тестирования";
