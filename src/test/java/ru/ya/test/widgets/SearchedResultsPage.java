57c4c0a62a9081c7659fa1e80372431cf168ea83
fettqa <fettqa@gmail.com>
2020-01-28 14:34:40 +0300

    UPD2: corrections, comparing of dates

--- src/test/java/ru/ya/test/widgets/SearchedResultsPage.java
+++ src/test/java/ru/ya/test/widgets/SearchedResultsPage.java
@@ -2,9 +2,7 @@ package ru.ya.test.widgets;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selenide.$;
-
 
 
 public class SearchedResultsPage {
@@ -12,8 +10,6 @@ public class SearchedResultsPage {
     private final SelenideElement mainPage = $("body.b-page>div.main").waitUntil(Condition.visible,20000);
 
     public void assertResults(final String condition) {
-
         mainPage.$("ul[aria-label=\"Результаты поиска\"]");
-
     }
 }
--- src/test/java/ru/yandex/widgets/MainYandexPage.java
+++ src/test/java/ru/yandex/widgets/MainYandexPage.java
@@ -2,16 +2,19 @@ package ru.yandex.widgets;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.Assert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class MainYandexPage {
     private final SelenideElement mainYandexPage = $("body.b-page").waitUntil(Condition.visible,20000);
+    private SimpleDateFormat dateFormat;
     private String dateNow;
     private String dateFromPage;
-
 
     public MainYandexPage checkNewsBlock(){
         mainYandexPage.$("#wd-wrapper-_topnews");
@@ -24,13 +27,14 @@ public class MainYandexPage {
     }
 
     public void checkDateTime(){
-
         dateFromPage = mainYandexPage.$("span.datetime[data-bem]").getAttribute("data-bem");
-        dateNow = new Date().toString();
-        System.out.println(dateFromPage);
-        System.out.println(dateNow);
+        Assert.assertTrue(compareWithCurrentDate(dateFromPage));
+    }
 
-
+    private boolean compareWithCurrentDate(final String dateFromPage){
+        dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+        dateNow = dateFormat.format(new Date());
+        return dateFromPage.contains(dateNow);
     }
 
 }
